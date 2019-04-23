3b62ee45ff329c5a43a6109731f7e5e981263932
anton <anton@morbax.com>
2019-04-23 14:39:44 +0300

    first & last user name

--- main/api/serializers.py
+++ main/api/serializers.py
@@ -9,7 +9,7 @@ User = get_user_model()
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ('id', 'username', 'email')
+        fields = ('id', 'username', 'email', 'first_name', 'last_name')
 
     # def create(self, validated_data):
     #     user = User(
